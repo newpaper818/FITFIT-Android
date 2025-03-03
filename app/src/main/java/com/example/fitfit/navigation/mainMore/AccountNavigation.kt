276cd4934a24a7a7ee013dd53aa4eac6b610988b
sejong <snm40109@gmail.com>
2025-03-03 20:39:54 +0900

    ui fix: accountScreen remove left padding

--- app/src/main/java/com/example/fitfit/navigation/mainMore/AccountNavigation.kt
+++ app/src/main/java/com/example/fitfit/navigation/mainMore/AccountNavigation.kt
@@ -56,29 +56,17 @@ fun NavGraphBuilder.accountScreen(
         val widthSizeClass = externalState.windowSizeClass.widthSizeClass
         val heightSizeClass = externalState.windowSizeClass.heightSizeClass
 
-        Row {
-            if (widthSizeClass == WindowWidthSizeClass.Compact) {
-                MySpacerRow(width = 0.dp)
-            } else if (
-                heightSizeClass == WindowHeightSizeClass.Compact
-                || widthSizeClass == WindowWidthSizeClass.Medium
-            ) {
-                MySpacerRow(width = NAVIGATION_RAIL_BAR_WIDTH)
-            } else if (widthSizeClass == WindowWidthSizeClass.Expanded) {
-                MySpacerRow(width = NAVIGATION_DRAWER_BAR_WIDTH)
-            }
 
-            AccountRoute(
-                use2Panes = externalState.windowSizeClass.use2Panes,
-                userData = appUiState.appUserData!!,
-                internetEnabled = externalState.internetEnabled,
-                spacerValue = externalState.windowSizeClass.spacerValue,
-                navigateToEditAccount = {/**TODO*/},
-                navigateToDeleteAccount = {/**TODO*/},
-                navigateUp = navigateUp,
-                onSignOutDone = {/**TODO*/},
-                modifier = modifier
-            )
-        }
+        AccountRoute(
+            use2Panes = externalState.windowSizeClass.use2Panes,
+            userData = appUiState.appUserData!!,
+            internetEnabled = externalState.internetEnabled,
+            spacerValue = externalState.windowSizeClass.spacerValue,
+            navigateToEditAccount = {/**TODO*/},
+            navigateToDeleteAccount = {/**TODO*/},
+            navigateUp = navigateUp,
+            onSignOutDone = {/**TODO*/},
+            modifier = modifier
+        )
     }
 }
